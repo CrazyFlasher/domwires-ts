b2fd27b15d81e676f2f66abfd4891aa42ff81473
CrazyFlasher <ms8LhrU9>
2021-10-17 13:51:00 +0300

    - Object.setPrototypeOf(this, AbstractModel) to AbstractModel

--- src/com/domwires/core/mvc/model/AbstractModel.ts
+++ src/com/domwires/core/mvc/model/AbstractModel.ts
@@ -1,7 +1,12 @@
 import {AbstractHierarchyObject} from "../hierarchy/AbstractHierarchyObject";
 import {IModel} from "./IModel";
+import {postConstruct} from "inversify";
 
 export abstract class AbstractModel extends AbstractHierarchyObject implements IModel
 {
-
+    @postConstruct()
+    protected init(): void
+    {
+        Object.setPrototypeOf(this, AbstractModel);
+    }
 }
